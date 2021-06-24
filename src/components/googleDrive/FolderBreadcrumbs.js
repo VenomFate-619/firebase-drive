782716fae20fb706f490c430a0c9789bbeb1d4ee
Aman Ali <amanali@pop-os.localdomain>
2021-06-24 13:17:49 +0530

    New:breadcrumbs added

--- src/components/googleDrive/FolderBreadcrumbs.js
+++ src/components/googleDrive/FolderBreadcrumbs.js
@@ -4,12 +4,29 @@ import { Link } from "react-router-dom";
 import { ROOT_FOLDER } from "../../hooks/useFolder";
 
 function FolderBreadcrumbs({ currentFolder }) {
+  let path = currentFolder === ROOT_FOLDER ? [] : [ROOT_FOLDER]
+  if (currentFolder) path = [...path, ...currentFolder.path]
   return (
     <Breadcrumb
     className="flex-grow-1"
-    listProps={{ className: "bg-white pl-0 m-0" }}
+    listProps={{ className: "bg-white  m-0" }}
     >
-
+      {path.map((folder, index) => (
+        <Breadcrumb.Item
+          key={folder.id}
+          linkAs={Link}
+          linkProps={{
+            to: {
+              pathname: folder.id ? `/folders/${folder.id}` : "/",
+              state: { folder: { ...folder, path: path.slice(1, index) } },
+            },
+          }}
+          className="text-truncate d-inline-block"
+          style={{ maxWidth: "150px" }}
+        >
+          {folder.name}
+        </Breadcrumb.Item>
+      ))}
       {currentFolder && (
         <Breadcrumb.Item
           className="text-truncate d-inline-block"
